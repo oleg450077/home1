70ec54c2da5af55c70e4eec8b152acb82fb36ad6
Oleg Luks <oleg450077@gmail.com>
2020-01-27 14:54:12 -0800

    One More Commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,5 +2,8 @@ public class TestClass {
 
     public static void main(String[]args){
         System.out.println("This is the end");
+        System.out.println("Commit for new_feature branch");
     }
+
+
 }
